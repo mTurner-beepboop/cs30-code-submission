19adcd85fc51ebf46c23a766d3a4f2d353b3416c
2320395g <2320395g@student.gla.ac.uk>
2021-01-09 16:06:28 +0000

    Fixed some links that weren't using the url template tag.

--- cs30_webapp/cs30_webapp/settings.py
+++ cs30_webapp/cs30_webapp/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'cs30_webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
