b35e389a511cd3c9c2b84d67e2428eaeb9ebce3f
Mark Turner <2386300T@student.gla.ac.uk>
2021-01-26 12:40:13 +0000

    Update feature now works

--- cs30_webapp/api/serializers.py
+++ cs30_webapp/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils import timezone
 from api.models import FlatfileEntry, NavigationInfo, CalculationInfo, OtherInfo
 
 class NavigationSerializer(serializers.ModelSerializer):
@@ -43,5 +44,38 @@ class ApiSerializer(serializers.ModelSerializer):
         return entry
         
     #TODO: write update method
-    def update(self, valid_data):
-        return
+    def update(self, instance, valid_data):
+        instance.ref_num = valid_data.pop('ref_num') #ref_number likely won't need changed
+        
+        #Get old information
+        nav_info = instance.navigation_info #unlikely that navigation info will be chagned too but keep functionality
+        calc_info = instance.calculation_info
+        other_info = instance.other_info
+        
+        #Get new information
+        nav_data = valid_data.pop('navigation_info')
+        calc_data = valid_data.pop('calculation_info')
+        other_data = valid_data.pop('other_info')
+        
+        #update
+        nav_info.scope = nav_data['scope']
+        nav_info.level_1 = nav_data['level1']
+        nav_info.level_2 = nav_data['level2']
+        nav_info.level_3 = nav_data['level3']
+        nav_info.level_4 = nav_data['level4']
+        nav_info.level_5 = nav_data['level5']
+        
+        calc_info.ef = calc_data['ef']
+        calc_info.cu = calc_data['cu']
+        
+        other_info.preference = other_data['preference']
+        other_info.source = other_data['source']
+        other_info.last_updated = timezone.now #should default to current time
+        
+        #save
+        nav_info.save()
+        calc_info.save()
+        other_info.save()
+        instance.save()
+
+        return instance
