0fab228a012d143d85de83a6b872c96250db036f
Mark Turner <2386300T@student.gla.ac.uk>
2021-01-27 13:39:55 +0000

    Put some work into some of team 29's requested function, namely the getting levels and scopes as lists

--- cs30_webapp/api/urls.py
+++ cs30_webapp/api/urls.py
@@ -4,4 +4,5 @@ from api import views
 urlpatterns = [
     url(r'^api/carbon$', views.entry_list),
     url(r'^api/carbon/(?P<pk>[0-9]+)$', views.entry_detail),
+    url(r'^api/carbon/scope$', views.categories),
 ]
--- cs30_webapp/api/views.py
+++ cs30_webapp/api/views.py
@@ -4,8 +4,8 @@ from django.http import HttpRequest
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
-from api.models import FlatfileEntry
-from api.serializers import ApiSerializer
+from api.models import FlatfileEntry, NavigationInfo, CalculationInfo, OtherInfo
+from api.serializers import ApiSerializer, NavigationSerializer
 
 @api_view(['GET','POST','DELETE'])
 def entry_list(request):
@@ -29,6 +29,10 @@ def entry_list(request):
         return JsonResponse(api_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE': #Delete all entries - likely not needed in final implementation
         count = FlatfileEntry.objects.all().delete()
+        #delete artifacts from nav, calc and other info
+        NavigationInfo.objects.all().delete()
+        CalculationInfo.objects.all().delete()
+        OtherInfo.objects.all().delete()
         return JsonResponse({'message': '{} entires were deleted'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
     
     
@@ -54,3 +58,40 @@ def entry_detail(request, pk):
     elif request.method == 'DELETE': #Deletes entry by id
         entry.delete()
         return JsonResponse({'message': 'Entry was deleted'}, status=status.HTTP_204_NO_CONTENT)
+        
+@api_view(['GET','POST'])
+def categories(request):
+    if request.method == 'GET': #Complete
+        #Return a list of scopes
+        dict_scope_list = list(NavigationInfo.objects.all().values('scope').distinct())
+        scopes = []
+        for dict in dict_scope_list:
+            scopes.append(dict['scope'])
+        return JsonResponse({'message':scopes})
+    if request.method == 'POST':
+        #Return list of possible levels
+        path = request.data
+        levels = ['scope','level1','level2','level3','level4','level5']
+        nav_data = NavigationInfo.objects.all()
+        i=0
+        for level in levels:
+            if level in path.keys():
+                i+=1
+                if level == 'scope':
+                    nav_data.filter(scope__icontains=path[level])
+                elif level == 'level1':
+                    nav_data.filter(level1__icontains=path[level])
+                elif level == 'level2':
+                    nav_data.filter(level2__icontains=path[level])
+                elif level == 'level3':
+                    nav_data.filter(level3__icontains=path[level])
+                elif level == 'level4':
+                    nav_data.filter(level4__icontains=path[level])
+                elif level == 'level5':
+                    nav_data.filter(level5__icontains=path[level])
+                continue
+            break
+        #nav_data should now contain only those entries down the path specified in the request
+        #Now find all unique levels below
+        level_vals = list(nav_data.values(levels[i]).distinct())
+        return JsonResponse({'message':level_vals})
