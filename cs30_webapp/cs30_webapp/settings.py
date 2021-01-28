dd6f2743b35e68feea872853e444a838b683cd03
2320395g <2320395g@student.gla.ac.uk>
2021-01-28 13:44:57 +0000

    Added STATIC_ROOT to settings to appease Heroku.

--- cs30_webapp/cs30_webapp/settings.py
+++ cs30_webapp/cs30_webapp/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = STATIC_DIR
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
